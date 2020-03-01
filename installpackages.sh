6e34e805fa2d8234dc343b43b7de191b76ec4c56
Raphael Brand <raphael-brand@@users.noreply.github.com>
2020-03-01 08:56:41 +0100

    update packages

---
+++ installpackages.sh
@@ -0,0 +1,7 @@
+npm remove jest-dom@3.5.0
+npm remove popper.js@1.16.1
+npm install --save-dev @testing-library/jest-dom
+npm install --save-dev @testing-library/dom
+npm install --save-dev @testing-library/react
+npm install --save-dev @popperjs/core
+npm install --save-dev @babel/cli @babel/core @babel/plugin-syntax-dynamic-import @babel/preset-env @babel/preset-flow @babel/preset-react @storybook/addon-actions @storybook/addon-links @storybook/addons @storybook/react babel-eslint babel-loader babel-plugin-module-resolver babel-plugin-styled-components eslint eslint-config-airbnb eslint-config-prettier eslint-import-resolver-babel-module eslint-plugin-filenames eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks flow-bin flow-typed http-server husky jest jest-dom jest-styled-components lint-staged node parcel-bundler parcel-plugin-bundle-visualiser prettier react-testing-library rimraf stylelint stylelint-config-prettier stylelint-config-standard stylelint-config-styled-components stylelint-processor-styled-components
